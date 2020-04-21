6d2180044df471046f1e30386a66b90afe2127af
Peerapong Maitriwong <peerapong@central.tech>
2020-04-21 13:53:47 +0700

    02

--- src/index.js
+++ src/index.js
@@ -2,15 +2,29 @@ const txtCountEl = document.querySelector('#txt-count');
 const btnIncrementEl = document.querySelector('#btn-increment');
 const btnDecrementEl = document.querySelector('#btn-decrement');
 const btnResetEl = document.querySelector('#btn-reset');
+const inputStepEl = document.querySelector('#input-step');
+const inputMinEl = document.querySelector('#input-min');
+const inputMaxEl = document.querySelector('#input-max');
 
 btnIncrementEl.addEventListener('click', () => {
-  txtCountEl.textContent = parseInt(txtCountEl.textContent) + 1;
+  const number =
+    parseInt(txtCountEl.textContent) + parseInt(inputStepEl.value || '1');
+  const max = parseInt(inputMaxEl.value || '0');
+
+  txtCountEl.textContent = Math.min(number, max);
 });
 
 btnDecrementEl.addEventListener('click', () => {
-  txtCountEl.textContent = parseInt(txtCountEl.textContent) - 1;
+  const number =
+    parseInt(txtCountEl.textContent) - parseInt(inputStepEl.value || '1');
+  const min = parseInt(inputMinEl.value || '0');
+
+  txtCountEl.textContent = Math.max(number, min);
 });
 
 btnResetEl.addEventListener('click', () => {
   txtCountEl.textContent = 0;
+  inputMinEl.value = 0;
+  inputMaxEl.value = 10;
+  inputStepEl.value = 1;
 });
