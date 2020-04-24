474ffabedf7753534858c25fd9666ea6b0ef30f6
Peerapong Maitriwong <peerapong@central.tech>
2020-04-24 16:06:23 +0700

    04

--- src/index.js
+++ src/index.js
@@ -11,6 +11,33 @@ function setState(nextState) {
   render();
 }
 
+const actions = {
+  increment() {
+    const number = state.count + state.step;
+    const max = state.max;
+
+    setState({ count: Math.min(number, max) });
+  },
+  decrement() {
+    const number = state.count - state.step;
+    const min = state.min;
+
+    setState({ count: Math.max(number, min) });
+  },
+  reset() {
+    setState({ count: 0, inputMaxEl: 10, inputMinEl: 0, inputStepEl: 1 });
+  },
+  changeStep(step) {
+    setState({ step });
+  },
+  changeMin(min) {
+    setState({ min });
+  },
+  changeMax(max) {
+    setState({ max });
+  },
+};
+
 const txtCountEl = document.querySelector('#txt-count');
 const btnIncrementEl = document.querySelector('#btn-increment');
 const btnDecrementEl = document.querySelector('#btn-decrement');
@@ -26,35 +53,13 @@ function render() {
   inputStepEl.value = state.step;
 }
 
-
-btnIncrementEl.addEventListener('click', () => {
-  const number = state.count + state.step;
-  const max = state.max;
-
-  setState({ count: Math.min(number, max) });
-});
-
-btnDecrementEl.addEventListener('click', () => {
-  const number = state.count - state.step;
-  const min = state.min;
-
-  setState({ count: Math.max(number, min) });
-});
-
-btnResetEl.addEventListener('click', () => {
-  setState({ count: 0, inputMaxEl: 10, inputMinEl: 0, inputStepEl: 1 });
-});
-
-inputStepEl.addEventListener('change', e => {
-  setState({ step: +e.target.value });
-});
-
-inputStepEl.addEventListener('change', e => {
-  setState({ min: +e.target.value });
-});
-
-inputStepEl.addEventListener('change', e => {
-  setState({ max: +e.target.value });
-});
+btnIncrementEl.addEventListener('click', actions.increment);
+btnDecrementEl.addEventListener('click', actions.decrement);
+btnResetEl.addEventListener('click', actions.reset);
+inputStepEl.addEventListener('change', e =>
+  actions.changeStep(+e.target.value),
+);
+inputMinEl.addEventListener('change', e => actions.changeMin(+e.target.value));
+inputMaxEl.addEventListener('change', e => actions.changeMax(+e.target.value));
 
 render();
