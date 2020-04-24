9c0fa8ec66d7b6ca67547c681ba46b37796a5edd
Peerapong Maitriwong <peerapong@central.tech>
2020-04-24 16:11:15 +0700

    05

--- src/index.js
+++ src/index.js
@@ -36,18 +36,37 @@ const actions = {
   changeMax(max) {
     setState({ max });
   },
+  maxCount() {
+    setState({ count: state.max });
+  },
+  minCount() {
+    setState({ count: state.min });
+  },
 };
 
 const txtCountEl = document.querySelector('#txt-count');
+const txtRemainingEl = document.querySelector('#txt-remaining');
 const btnIncrementEl = document.querySelector('#btn-increment');
 const btnDecrementEl = document.querySelector('#btn-decrement');
 const btnResetEl = document.querySelector('#btn-reset');
+const btnMinEl = document.querySelector('#btn-min');
+const btnMaxEl = document.querySelector('#btn-max');
 const inputStepEl = document.querySelector('#input-step');
 const inputMinEl = document.querySelector('#input-min');
 const inputMaxEl = document.querySelector('#input-max');
 
 function render() {
+  const txtCountClassName =
+    state.count > 0.75 * state.max
+      ? 'text-success'
+      : state.count > 0.5 * state.max
+      ? 'text-warning'
+      : 'text-danger';
+  const remainingCount = state.max - state.count;
+
+  txtCountEl.className = txtCountClassName;
   txtCountEl.textContent = state.count;
+  txtRemainingEl.textContent = remainingCount;
   inputMinEl.value = state.min;
   inputMaxEl.value = state.max;
   inputStepEl.value = state.step;
@@ -56,6 +75,8 @@ function render() {
 btnIncrementEl.addEventListener('click', actions.increment);
 btnDecrementEl.addEventListener('click', actions.decrement);
 btnResetEl.addEventListener('click', actions.reset);
+btnMinEl.addEventListener('click', actions.minCount);
+btnMaxEl.addEventListener('click', actions.maxCount);
 inputStepEl.addEventListener('change', e =>
   actions.changeStep(+e.target.value),
 );
