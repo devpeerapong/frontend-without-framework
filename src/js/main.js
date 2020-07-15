bdb4b8b3bc94bddd60d12ac4301d81b048d215c2
Peerapong Maitriwong <work@peerapong.dev>
2020-07-15 23:39:04 +0700

    added input step

--- src/js/main.js
+++ src/js/main.js
@@ -3,13 +3,20 @@ const btnMaxDOM = document.querySelector("#btn-max");
 const btnIncrementDOM = document.querySelector("#btn-inc");
 const btnDecrementDOM = document.querySelector("#btn-dec");
 const txtCountDOM = document.querySelector("#txt-count");
+const inputStepDOM = document.querySelector("#input-step");
 
 btnIncrementDOM.addEventListener("click", () => {
-  txtCountDOM.innerText = Math.min(10, parseInt(txtCountDOM.innerText) + 1);
+  txtCountDOM.innerText = Math.min(
+    10,
+    parseInt(txtCountDOM.innerText) + parseInt(inputStepDOM.value)
+  );
 });
 
 btnDecrementDOM.addEventListener("click", () => {
-  txtCountDOM.innerText = Math.max(0, parseInt(txtCountDOM.innerText) - 1);
+  txtCountDOM.innerText = Math.max(
+    0,
+    parseInt(txtCountDOM.innerText) - parseInt(inputStepDOM.value)
+  );
 });
 
 btnMinDOM.addEventListener("click", () => {
